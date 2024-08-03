d899a8191904e730b27b4ee813a384f9c36fe769
zaku <jakub.jacqes@gmail.com>
2024-08-03 23:45:31 +0200

    Adding lab2 task 2

--- lab2/zad2/zad2.c
+++ lab2/zad2/zad2.c
@@ -2,5 +2,18 @@
 
 int main(){
     
+    unsigned long number;
+    int sum=0,rem;
+    
+    printf("Podaj potezna liczbe: ");   
+    scanf("%lu", &number);
 
-}
+    while(number != 0){
+        rem = number%10;
+        sum = sum + rem;
+        number = number/10;
+    }
+    
+    printf("Suma cyfr: %d\n",sum);
+    
+    }
